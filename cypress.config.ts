cb97c01180b95a09e1b98254c2bcfdbc02088aee
Kareem-Ali02 <kareem.business02@gmail.com>
2025-05-12 16:48:56 +0300

    integrating the CI github action

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,19 +2,31 @@ import { defineConfig } from "cypress";
 import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
 import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
+import mochawesome from "cypress-mochawesome-reporter/plugin";
 import fs from "fs"; // to write to fixture
 
 export default defineConfig({
   e2e: {
     baseUrl: "https://www.automationexercise.com/",
     specPattern: "**/*.feature",
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: "Automation Exercise Test Report",
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      overwrite: false,
+      quiet: true,
+      html: true,
+      json: true,
+      reportDir: "cypress/reports/mochawesome",
+    },
 
-    async setupNodeEvents(
-      on: Cypress.PluginEvents,
-      config: Cypress.PluginConfigOptions
-    ): Promise<Cypress.PluginConfigOptions> {
+    async setupNodeEvents(on, config) {
+      // Cucumber BDD plugin
       await addCucumberPreprocessorPlugin(on, config);
 
+      // esbuild preprocessor for .feature steps
       on(
         "file:preprocessor",
         createBundler({
@@ -22,6 +34,10 @@ export default defineConfig({
         })
       );
 
+      // Mochawesome reporter plugin
+      mochawesome(on);
+
+      // Custom task to write fixture file
       on("task", {
         writeUserData(data) {
           fs.writeFileSync(
@@ -30,7 +46,6 @@ export default defineConfig({
           );
           return null;
         },
-        
       });
 
       return config;
